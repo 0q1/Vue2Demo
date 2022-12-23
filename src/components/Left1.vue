29c03a104ba5fdfb877318a1f4aa7d5e28e5984d
0q1 <765531054@qq.com>
2022-12-23 12:28:44 +0800

    m

--- src/components/Left1.vue
+++ src/components/Left1.vue
@@ -24,6 +24,7 @@ export default{
             //指令第一次创建时调用
             bind(el,binding){
                 el.style.color='red'
+                console.log("v-color")
             },
             update(el,binding){
                 el.value = binding.value
